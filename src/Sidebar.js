71eb702cb79d0ecdf9028905fc9e39e899a5ae0c
nstwebd <nst.webd@gmail.com>
2021-06-12 22:13:10 +0530

    updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React from "react";
+import { MdDelete } from "react-icons/md";
 
-const Sidebar = ({notes, onAddNote, onDeleteNote, activeNote, setActiveNote}) => {
-    const sortedNotes = notes.sort((a,b) => b.lastModified-a.lastModified)
-    return (
-
-        <div className="app-sidebar">
-            <div className="app-sidebar-header">
-                <h1>Notes</h1>
-                <button onClick={onAddNote}>Add</button>
+const Sidebar = ({
+  notes,
+  onAddNote,
+  onDeleteNote,
+  activeNote,
+  setActiveNote,
+}) => {
+  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button onClick={onAddNote}>Add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        {sortedNotes.map((note) => (
+          <div
+            className={`app-sidebar-note ${note.id === activeNote && "active"}`}
+            onClick={() => setActiveNote(note.id)}
+            key={note.id}
+          >
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button onClick={() => onDeleteNote(note.id)}>
+                <MdDelete style={{ fontSize: "18px" }} />
+              </button>
             </div>
-            <div className="app-sidebar-notes">
-                {sortedNotes.map(note => (
-                    <div className={`app-sidebar-note ${note.id === activeNote && 'active'}`} onClick={() => setActiveNote(note.id)} key={note.id}>
-                        <div className="sidebar-note-title">
-                            <strong>{note.title}</strong>
-                            <button onClick={()=>onDeleteNote(note.id)}>Delete</button>
-                        </div>
-                        <p>{note.body && note.body.substr(0,100) +'...'}</p>
-                        <small className="note-meta">
-                            Last Modified :{new Date(note.lastModified).toLocaleDateString("en-GB", {
-                                hour:'2-digit',
-                                minute:'2-digit'
-                        })}
-                        </small>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
+            <p>{note.body && note.body.substr(0, 100) + "..."}</p>
+            <small className="note-meta">
+              Last Modified :
+              {new Date(note.lastModified).toLocaleDateString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
